1119316827f7eceea715991b9ff301a9f9cbb057
Nelson Ochieng Oranga <oranganelson19@gmail.com>
2025-10-23 13:36:20 +0300

    Add files via upload

---
+++ oranganelson_portfolio/assets/js/script.js
@@ -0,0 +1,10 @@
+const faders = document.querySelectorAll('.fade-in');
+const appearOptions = { threshold: 0.2 };
+const appearOnScroll = new IntersectionObserver(function(entries, observer) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) return;
+    entry.target.classList.add('appear');
+    observer.unobserve(entry.target);
+  });
+}, appearOptions);
+faders.forEach(fader => appearOnScroll.observe(fader));
